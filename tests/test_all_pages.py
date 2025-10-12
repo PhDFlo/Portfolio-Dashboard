52c95d2c6409b6970cf7551f756066da2b8e1687
PhDFlo <florian.granger43@gmail.com>
2025-10-12 20:45:40 +0200

    add streamlit actions

--- tests/test_all_pages.py
+++ tests/test_all_pages.py
@@ -29,7 +29,6 @@ class TestMultiPageApp(unittest.TestCase):
 
         for filename in pages:
             with self.subTest(filename=filename):
-                os.chdir(str(Path(__file__).parent.parent))
                 # Initialize the app test with the main app
                 at = AppTest.from_file("dashboard.py").run()
 
