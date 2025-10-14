6d4234c4c2256a980a63eb6b453f82c31f9864e7
Florian Granger <florian.granger43@gmail.com>
2025-10-14 09:08:19 +0200

    Add increment test on security comparator page

--- tests/test_security_comparator.py
+++ tests/test_security_comparator.py
@@ -2,6 +2,7 @@ import os
 import sys
 import unittest
 from pathlib import Path
+import pytest
 
 from streamlit.testing.v1.app_test import AppTest
 
@@ -9,7 +10,7 @@ from streamlit.testing.v1.app_test import AppTest
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 
-class TestMultiPageApp(unittest.TestCase):
+class TestSecurityComparator(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
         self.page_file = "pages/compare_securities.py"
@@ -25,6 +26,29 @@ class TestMultiPageApp(unittest.TestCase):
 
     def test_increment_and_add(self):
         """A user increments the number input, then clicks Add"""
+        at = AppTest.from_file(self.page_file).run()
+
+        # Initial A
+        for i in range(12):
+            at.number_input[i].increment().run()
+            print(at.number_input[i].value)
+
+        # Better assertion to be found
+        assert at.number_input[0].value == pytest.approx(10000.01, 0.001)
+        assert at.number_input[1].value == pytest.approx(0.07, 0.001)
+        assert at.number_input[2].value == pytest.approx(0.01, 0.001)
+        assert at.number_input[3].value == pytest.approx(0.015, 0.001)
+        assert at.number_input[4].value == pytest.approx(0.015, 0.001)
+        assert at.number_input[5].value == pytest.approx(0.182, 0.001)
+        assert at.number_input[6].value == pytest.approx(10000.01, 0.001)
+        assert at.number_input[7].value == pytest.approx(0.09, 0.001)
+        assert at.number_input[8].value == pytest.approx(0.01, 0.001)
+        assert at.number_input[9].value == pytest.approx(0.015, 0.001)
+        assert at.number_input[10].value == pytest.approx(0.015, 0.001)
+        assert at.number_input[11].value == pytest.approx(0.31, 0.001)
+
+    def test_run_comparison(self):
+        """Run security comparison after incrementing number input"""
         at = AppTest.from_file(self.page_file).run()
         at.number_input[0].increment().run()
         at.button[0].click().run()
