6814d86f63bb8bb853361a4c7675d4c5e2dadc3f
Florian Granger <florian.granger43@gmail.com>
2025-11-03 19:21:26 +0100

    Add ticker list and get default values from portfolio

--- pages/load_portfolio.py
+++ pages/load_portfolio.py
@@ -62,6 +62,9 @@ else:
             "Number Held": [0.0],
         }
     )
+   
+# List of tickers for buy and sell 
+ticker_options = [""] + [security.ticker for security in st.session_state.portfolio.securities]
 
 st.subheader("Security List")
 
@@ -90,34 +93,61 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.subheader("Buy Security")
-    ticker_input = st.text_input("Security Ticker")
+    ticker_input_buy = st.selectbox(
+        "Security ticker",
+        options=ticker_options,
+        key="ticker_buy_choice",
+        index=1
+        if len(ticker_options) > 1 else 0,
+    )
     quantity = st.number_input("Quantity to Buy", value=1.0, format="%.1f", step=1.0)
-    currency = st.text_input("Security Currency", value="EUR")
-    buy_price = st.number_input("Unit Price", format="%.2f")
+    
+    # Set default currency and value based on selected ticker
+    default_currency = st.session_state.portfolio.currency
+    default_buy_price = 0.0
+    for security in st.session_state.portfolio.securities:
+        if ticker_input_buy == security.ticker:
+            default_currency = security.currency
+            default_buy_price = security.price_in_security_currency
+            break
+    
+    currency = st.text_input("Security Currency", value=default_currency)
+    buy_price = st.number_input("Unit Price", value=default_buy_price, format="%.2f")
             
 with col2:
     st.subheader("Sell Security")
-    ticker_input_sell = st.text_input("Security Ticker to Sell", key="sell_ticker")
+    ticker_input_sell = st.selectbox(
+        "Security ticker to sell",
+        options=ticker_options,
+        key="ticker_sell_choice",
+        index=1
+        if len(ticker_options) > 1 else 0,
+    )
     quantity_sell = st.number_input(
         "Quantity to Sell", value=1.0, format="%.1f", step=1.0, key="sell_quantity"
     )
-            
+
+col1, col2 = st.columns(2)     
 with col1:
-    if st.button("💸 Buy Security"):
+    if st.button("📥 Buy Security",
+        key="buy_security",
+        use_container_width=True):
         try:
             st.session_state.portfolio.buy_security(
-                ticker=ticker_input,
+                ticker=ticker_input_buy,
                 quantity=quantity,
                 price=buy_price,
                 currency=currency,
             )
-            st.success(f"Bought {quantity} unit(s) of {ticker_input} at {buy_price}")
+            st.success(f"Bought {quantity} unit(s) of {ticker_input_buy} at {buy_price}")
             st.rerun()
         except Exception as e:
             st.error(f"Error buying security: {str(e)}")
             
 with col2:
-    if st.button("💵 Sell Security"):
+    if st.button("📤 Sell Security",
+        key="sell_security",
+        use_container_width=True):
         try:
             st.session_state.portfolio.sell_security(
                 ticker=ticker_input_sell,
